398e1d93bad1d1fa37885cf9839dca1482a2c473
Ayush <prakashayush414@gmail.com>
2026-03-03 11:04:00 +0530

    FCFS for the Simulation

---
+++ fcfs.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int no_proccess;
+    cout<<"Enter the No. of Proccess:";
+    cin>>no_proccess;
+    vector<int>pid;
+    vector<int>arrival_time;
+    vector<int>burst_time;
+    
+}
