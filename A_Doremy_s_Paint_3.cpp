b80b1df98c2dd491aaeb69cb488326a23bd9018d
Ayush <prakashayush414@gmail.com>
2026-03-22 14:53:20 +0530

    Adding Doremys Paint 3 file

---
+++ A_Doremy_s_Paint_3.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check_distinct(vector<int>arr)
+{
+    map<int,int>mp;
+    for (int i=0;i<arr.size();i++)
+    {
+        mp[arr[i]]++;
+    }
+
+    if (mp.size()>2)
+        return false;
+    if (mp.size()==1)
+        return true;
+    auto it=mp.begin();
+    int first=it->second;
+    it++;
+    int second=it->second;
+    if (abs(first-second)<=1)
+        return true;
+    else
+        return false;
+
+}
+int main ()
+{
+    int test;
+    cin>>test;
+    while (test--)
+    {
+        int size;
+        cin>>size;
+        vector<int>arr(size,0);
+        for (int i=0;i<size;i++)
+        {
+            cin>>arr[i];
+        }
+        bool distinct=check_distinct(arr);
+        if(distinct)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+    }
+}
