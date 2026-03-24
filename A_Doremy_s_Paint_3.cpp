d8d3efcca9f37552dbce7ebf331102167721d50f
Ayush <prakashayush414@gmail.com>
2026-03-24 22:41:37 +0530

    Adding Dont try to count

--- A_Doremy_s_Paint_3.cpp
+++ A_Doremy_s_Paint_3.cpp
@@ -33,6 +33,7 @@ int main ()
         vector<int>arr(size,0);
         for (int i=0;i<size;i++)
         {
+            
             cin>>arr[i];
         }
         bool distinct=check_distinct(arr);
